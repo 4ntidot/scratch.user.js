ee352633b33b50ce291417c4aca886fc60bafe18
fowled <ma15fo43@gmail.com>
2022-01-26 13:29:42 +0100

    🐛 Changed API endpoint to resolve a bug

--- userscript.user.js
+++ userscript.user.js
@@ -12,41 +12,43 @@
 (async function () {
     'use strict';
 
-    const oldNavbarVersion = document.getElementById("pagewrapper");
-    const user = document.title.split(" ")[0];
-    const url = window.location.href;
+    window.onload = function () {
+        const oldNavbarVersion = document.getElementById("pagewrapper");
+        const user = document.title.split(" ")[0];
+        const url = window.location.href;
 
-    if (!oldNavbarVersion) {
-        document.getElementsByTagName("li")[4].innerHTML = '<a href="/discuss/15">Forum</a>';
-    } else {
-        document.getElementsByTagName("li")[3].innerHTML = '<a href="/discuss/15">Forum</a>';
-    }
+        if (!oldNavbarVersion) {
+            document.getElementsByTagName("li")[4].innerHTML = '<a href="/discuss/15">Forum</a>';
+        } else {
+            document.getElementsByTagName("li")[3].innerHTML = '<a href="/discuss/15">Forum</a>';
+        }
 
-    if (url.includes("/users/")) {
-        showID(user);
-        followersCount(user);
-    }
+        if (url.includes("/users/")) {
+            showID(user);
+            followersCount(user);
+        }
 
-    async function showID(user) {
-        fetch(`https://scratchdb.lefty.one/v3/user/info/${user}`)
-            .then(response => response.json())
-            .then(data => {
-                const location = data.country;
-                const id = data.id;
+        async function showID(user) {
+            fetch(`https://scratchdb.lefty.one/v2/user/info/${user}`)
+                .then(response => response.json())
+                .then(data => {
+                    const location = data.country;
+                    const id = data.id;
 
-                document.getElementsByClassName("location")[0].innerHTML = `${location} • ${id}`;
-            });
-    }
+                    document.getElementsByClassName("location")[0].innerHTML = `${location} • ${id}`;
+                });
+        }
 
-    async function followersCount(user) {
-        fetch(`https://scratchdb.lefty.one/v3/user/info/${user}`)
-            .then(response => response.json())
-            .then(data => {
-                const followingCount = data.statistics.following;
-                const followersCount = data.statistics.followers;
+        async function followersCount(user) {
+            fetch(`https://scratchdb.lefty.one/v2/user/info/${user}`)
+                .then(response => response.json())
+                .then(data => {
+                    const followingCount = data.following;
+                    const followersCount = data.followers;
 
-                document.getElementsByClassName("box-head")[5].innerHTML = `<h4>Following (${followingCount})</h4>`;
-                document.getElementsByClassName("box-head")[6].innerHTML = `<h4>Followers (${followersCount})</h4>`;
-            });
+                    document.getElementsByClassName("box-head")[5].innerHTML = `<h4>Following (${followingCount})</h4>`;
+                    document.getElementsByClassName("box-head")[6].innerHTML = `<h4>Followers (${followersCount})</h4>`;
+                });
+        }
     }
 })();
